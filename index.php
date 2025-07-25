e19b0efa6e6e8adaa6c769dc0ea745c625485f3c
Gavin Chen <gavinchen1003@gmail.com>
2025-07-24 20:35:10 -0400

    modified:   index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 $file=file_get_contents("./content.html");
+
+$copyYear = 2019; 
+$curYear = date('Y');
+$file=str_replace('&copy; 2019 - 2024', '.&copyYear.(($copyYear != $curYear') ? ' - '.$curYear : ''), $file);
+
 $combine=$file;
 
 $combine = preg_replace('/<!--.*?-->/ms','',$combine);
