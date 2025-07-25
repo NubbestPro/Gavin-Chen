825a543087b387ca89222593af2b1c07e71dd63a
Gavin Chen <gavinchen1003@gmail.com>
2025-07-24 20:16:04 -0400

    modified:   index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 $file=file_get_contents("./content.html");
 $combine=$file;
 
+$combined = preg_replace('/<!--.*?-->/ms','',$combined);
+$combined = preg_replace('/\s+/', ' ', $combined);
+$combined = preg_replace('~>\s+<~', '><', $combined);
+
 echo $combine;
 exit;
 ?>
